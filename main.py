7faccffd0ab6fef03105becc9cb8ad8d0089dd00
AZ_Naeimi <zahranaiemi21@gmail.com>
2025-11-15 12:27:30 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 stock_price={"AAPL":180,"TSLA":250,"GOOG":150,"AMZN":140}
 total_investment=0
-
+portfolio=[]
 while True:
     stock = input("Enter stock name (or 'DONE' to finish):").upper()
     if stock=="DONE":
@@ -8,8 +8,26 @@ while True:
     if stock not  in stock_price:
         print(" stock not found.Try again")
         continue
-    quantity = int(input(f"Enter quantity of {stock}:"))
-    total_investment+=(stock_price[stock]*quantity)
-    print(f" The total_investment is :${total_investment}")
+    try:
+       quantity = int(input(f"Enter quantity of {stock}:"))
+       if quantity<=0:
+            print("Quantity must be positive!")
+            continue
+    except ValueError:
+        print("please enter a valid number")
+        continue
+
+    investment=stock_price[stock]*quantity
+    portfolio.append((stock,quantity,investment))
+    total_investment+=investment
+    print(f"Total:{total_investment}")
+    with open ("portfolio.txt","a") as f:
+       f.write(f"{stock},{quantity},{investment}\n")
+    with open("portfolio.txt", "a") as f:
+     f.write(f" The total_investment is :${total_investment}")
+     f.write("\n")
+print("✅ Portfolio saved successfully!")
+
+
 
 
